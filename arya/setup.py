61b5f6c0253fd8c90c948ad3e4d76baca83883c0
Mike Timm <mtimm@cisco.com>
2014-05-23 12:14:03 -0700

    Add basic packaging

---
+++ arya/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('LICENSE') as f:
+    license = f.read()
+
+exec(open('arya/version.py').read())
+
+setup(
+    name='arya',
+    version=__version__,
+    description='APIC Rest to Pthon Adapter',
+    long_description=open('README.txt').read(),
+    packages=['arya'],
+    url='https://github.com/datacenter/ACI/arya',
+    license=license,
+    author='Paul Lesiak',
+    author_email='palesiak@cisco.com',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    scripts=['bin/arya.py', 'bin/getconfigfromapic.py'],
+)
+
