78fe96425bb89d862365235b243d9acdedfbd17f
yellowbang <astrotabon@gmail.com>
2014-08-25 15:01:36 -0700

    lab4 yaml

--- configuration-python/Lab4/create3TierApplicationYaml.py
+++ configuration-python/Lab4/create3TierApplicationYaml.py
@@ -5,29 +5,27 @@ from connectEpgContract import connect_epg_contract
 from utility import *
 
 
-def lab4(modir, tenant_name, application_name):
-    """Following the lab guide, we create 3Tiers application profile"""
-
-    # Create a application profile
-    create_application(modir, tenant_name, application_name)
-    # Create three EPGs: Web_EPG, App_EPG and DB_EPG
-    create_application_epg(modir, tenant_name, application_name, 'Web_EPG', bridge_domain=tenant_name+'_BD1')
-    create_application_epg(modir, tenant_name, application_name, 'App_EPG', bridge_domain=tenant_name+'_BD1')
-    create_application_epg(modir, tenant_name, application_name, 'DB_EPG', bridge_domain=tenant_name+'_BD1')
-    # Provide contracts between these EPGs
-    connect_epg_contract(modir, tenant_name, application_name, 'Web_EPG', 'provided', 'Web_Con')
-    connect_epg_contract(modir, tenant_name, application_name, 'App_EPG', 'provided', 'App_Con')
-    connect_epg_contract(modir, tenant_name, application_name, 'DB_EPG', 'provided', 'DB_Con')
-    connect_epg_contract(modir, tenant_name, application_name, 'Web_EPG', 'consumed', 'App_Con')
-    connect_epg_contract(modir, tenant_name, application_name, 'App_EPG', 'consumed', 'DB_Con')
-
-
 if __name__ == '__main__':
-    if len(sys.argv) != 6:
-        print 'Usage:', __file__, '<hostname> <username> <password> <tenant_name> <application_name>'
+
+    try:
+        data = read_config_yaml_file(sys.argv[1], login_info=False)
+    except IOError:
+        print 'No such file or directory:', sys.argv[1]
         sys.exit()
     else:
-        hostname, username, password, tenant_name, application_name = sys.argv[1:]
-        modir = apic_login(hostname, username, password)
-        lab4(modir, tenant_name, application_name)
-        modir.logout()
+        host_name, user_name, password = get_login_info(data)
+        tenant_name = data['tenant']
+        application = data['application']
+    modir = apic_login(host_name, user_name, password)
+
+    # create application
+    create_application(modir, tenant_name, application)
+    # create application EPG
+    for epg in data['application_epg']:
+        create_application_epg(modir, tenant_name, application, epg['name'], bridge_domain=epg['bridge_domain'])
+
+    # Provide contracts between these EPGs
+    for con in data['applied_contracts']:
+        connect_epg_contract(modir, tenant_name, application, con['applied_epg'], con['type'], con['name'])
+
+    modir.logout()
