8084ada6730be3f8a439ed28b07c04c7ae68e297
yellowbang <astrotabon@gmail.com>
2014-09-08 17:00:59 -0700

    add createAttachableAccessEntityprofile

--- configuration-python/ConfiguringInOutBandManagementAccess/createPhysicalDomain.py
+++ configuration-python/ConfiguringInOutBandManagementAccess/createPhysicalDomain.py
@@ -54,7 +54,6 @@ if __name__ == '__main__':
             # Try mode two: load a config file
             data, host_name, user_name, password = read_config_yaml_file(sys.argv[1])
             physical_domain = data['physical_domain']
-            optional_args = data['optional_args']
         except (IOError, KeyError, TypeError, IndexError) as input_error:
             # If both mode one and two fail, try mode three: wizard
             if len(sys.argv)>1:
@@ -62,7 +61,11 @@ if __name__ == '__main__':
             host_name, user_name, password = input_login_info()
             physical_domain = input_key_args()
             optional_args = input_optional_args()
-
+        else:
+            if 'optional_args' in data.keys():
+                optional_args = data['optional_args']
+            else:
+                optional_args = {}
     else:
         physical_domain = args.pop('physical_domain')
         optional_args = args
