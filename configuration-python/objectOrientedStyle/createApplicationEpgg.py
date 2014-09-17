ef5071bec57e728ab8eaf0ed87a9d12ccbb5d4b4
yellowbang <astrotabon@gmail.com>
2014-09-17 12:47:53 -0700

    add createApplicationEpg

--- configuration-python/objectOrientedStyle/createApplicationEpgg.py
+++ configuration-python/objectOrientedStyle/createApplicationEpgg.py
@@ -29,6 +29,7 @@ def create_application(fv_ap, epg, **args):
     if 'bridge_domain' in args.keys():
         fv_rsbd = RsBd(fv_aepg, tnFvBDName=args['bridge_domain'])
 
+
 class CreateApplicationEpg(CreateMo):
 
     def __init__(self):
@@ -39,26 +40,29 @@ class CreateApplicationEpg(CreateMo):
 
     def set_cli_mode(self):
         super(CreateApplicationEpg, self).set_cli_mode()
+        self.parser_cli.add_argument('application', help='Application Name')
         self.parser_cli.add_argument('epg', help='Application EPG Name')
         self.parser_cli.add_argument('-Q', '--QoS_class', dest='prio', default= DEFAULT_QOS, choices=QOS_CHOICES, help='The priority level of a sub application running behind an endpoint group.')
         self.parser_cli.add_argument('-b', '--bridge_domain', help='A relation to the bridge domain associated to this endpoint group.')
 
     def read_key_args(self):
+        self.application = self.args['application']
         self.epg = self.args['epg']
 
     def wizard_mode_input_args(self):
+        self.args['application'] = self.input_application_name()
         self.args['epg'] = input_key_args()
         if not self.delete:
             self.args['optional_args'] = input_optional_args()
 
     def delete_mo(self):
-        self.check_if_mo_exist('uni/tn-' + self.tenant + '/ap-', self.application, Ap, description='Application')
+        self.check_if_mo_exist('uni/tn-' + self.tenant + '/ap-' + self.application + '/epg-', self.epg, AEPg, description='Application EPG')
         super(CreateApplicationEpg, self).delete_mo()
 
     def main_function(self):
         # Query a tenant
-        fv_tenant = self.check_if_tenant_exist()
-        create_application(fv_tenant, self.application, optional_args=self.optional_args)
+        fv_ap = self.check_if_mo_exist('uni/tn-' + self.tenant + '/ap-', self.application, Ap, description='Application')
+        create_application(fv_ap, self.epg, optional_args=self.optional_args)
 
 if __name__ == '__main__':
     mo = CreateApplicationEpg()
